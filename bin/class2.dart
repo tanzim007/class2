43e80f6e6732bd4c00861b82c7ea157fb00b67bc
Tanzim Hossain <tanzim35-2700@diu.edu.bd>
2022-06-08 14:55:21 +0600

    changes

--- bin/class2.dart
+++ bin/class2.dart
@@ -3,18 +3,22 @@ import 'package:test/test.dart';
 
 void main() {
   /*List<String> items = List.generate(10, (index) => "Item ${index +1}");
+
   /*for (var item in items){
     print(item);
   } OR */
   items.forEach((element) => print(element));*/
+
+  /* 1.
   print('Initialized downloading process...');
   download('www.bigybigy.com', (msg) => print(msg));
-  print('End');
+  print('End');*/
 }
 
+/* 1.
 void download(String url, Function(String) callback){
   print('Start Downloading....');
   Future.delayed(Duration(seconds: 5),(){
     callback('Completed');
   });
-}
+}*/
