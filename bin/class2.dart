9576570c410b01388e7c4c32a8234ac0f470fe6a
Tanzim Hossain <tanzim35-2700@diu.edu.bd>
2022-06-08 14:50:55 +0600

    Initial commit

---
+++ bin/class2.dart
@@ -0,0 +1,20 @@
+import 'package:class2/class2.dart' as class2;
+import 'package:test/test.dart';
+
+void main() {
+  /*List<String> items = List.generate(10, (index) => "Item ${index +1}");
+  /*for (var item in items){
+    print(item);
+  } OR */
+  items.forEach((element) => print(element));*/
+  print('Initialized downloading process...');
+  download('www.bigybigy.com', (msg) => print(msg));
+  print('End');
+}
+
+void download(String url, Function(String) callback){
+  print('Start Downloading....');
+  Future.delayed(Duration(seconds: 5),(){
+    callback('Completed');
+  });
+}
